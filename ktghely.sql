81db9c1961fe1091111dd00bbf15559a09c3b825
triton-dev <szikoragy@gmail.com>
2017-01-03 13:08:16 +0100

    Create ktghely.sql

---
+++ ktghely.sql
@@ -0,0 +1,41 @@
+--
+-- SQL betöltő költséghely táblához
+--
+\copy ktghely from stdin
+11-110-0	Törzs Ügyvezető
+11-111-0	Törzs Ügyvezető
+11-120-0	MFO Üzemfenntartási Önálló Csop. (IT)
+11-200-0	GFO Gazdálkodási Főosztály
+11-210-0	GFO Humánügyi és Bérelszámolási Önálló C
+11-220-0	GFO Tervezési és Controlling Osztály
+11-230-0	GFO Pénzügyi és Számviteli Osztály
+11-250-0	GFO Értékesítési és Marketing Önálló Cso
+11-260-0	GFO Anyaggazdálkodási Osztály
+11-300-0	MFO Műszaki és Term.előkészitő Főosztály
+11-310-0	MFO Technológiai Osztály
+11-330-0	MFO Üzemfenntartási Önálló Csoport
+11-335-0	Minőségirányítási Csoport
+11-400-0	TFO Termelési Főosztály
+11-410-0	MFO Fejlesztési és Szerződés Előkészítés
+11-411-0	MFO Minőségellenőrzési Önálló Csoport
+12-420-0	TFO Járműjavitó Üzem
+12-430-0	TFO Fődarabjavító Üzem
+13-140-0	MFO Minőségellenőrzési Önálló Csoport
+13-333-0	MFO Üzemfenntartási Önálló Csoport (karb
+13-421-0	TFO Járműlakatos és Járműszerelő Művezet
+13-422-0	TFO Járműlakatos és Járműszerelő Művezet
+13-423-0	TFO Villanyszerelő Művezetőség
+13-424-0	TFO Fényező és Asztalos művezetőség fény
+13-425-0	TFO Fényező és Asztalos művezetőség aszt
+13-431-0	TFO Motorjavitó és Műszerész Művezetőség
+13-432-0	TFO Kerékpár- és Forgóvázjavító Műv
+13-433-0	TFO Kerékpár- és Forgóvázjavító Művezető
+13-441-0	TFO Alkatrész és Kitérőgyártó Művezetősé
+13-443-0	TFO Motorjavító és Műszerész Művezetőség
+13-444-0	TFO Alkatrész és Kitérőgyártó Művezetősé
+13-501-0	TFO Termelés Támogatási Önálló Csoport
+13-503-0	NEM TFO Termelési Főosztály
+14-460-0	Törzs Szakmunkásképző Csoport
+21-322-1	GFO Raktár
+21-322-3	GFO Raktár (Szerszámkiadó)
+\.
