4c0878fe578f247dfb737383f504c22bcef5edc8
triton-dev <szikoragy@gmail.com>
2017-01-03 12:05:22 +0100

    Create dolgozok.sql

---
+++ dolgozok.sql
@@ -0,0 +1,314 @@
+--
+-- SQL loader dolgozók táblához
+--
+\copy dolgozok(torzsszam, nev) from stdin
+93264	Szokolai Ferenc
+93267	Varnyu Gábor
+93268	Rajczi Gyula
+93269	Tamási Tibor
+93271	Marosán Mihály
+93272	Tóth Zoltán Károly
+93281	Szöllősi Tibor
+93284	Kele Károly
+93285	Bádi István
+93290	Katona László
+93291	Holló János
+93292	Buczynski Kazimierz Edward
+93293	Sebestyén Tibor
+93295	Csaja József
+93299	Kisvári Vendel
+93302	Várkonyi István
+93303	Erőss Mihály
+93304	Nagy István
+93306	Mityka György
+93308	Németh Béla
+93310	Varga Zoltán
+93312	Jakabovics Imre
+93313	Gellény Ottó
+93314	Csizmadia Zsigmond
+93318	Örkényi Zsolt
+93319	Kalmár István
+93330	Kis Zoltán Károly
+93338	Szabó János  Sándor
+93340	Barabás Lajos
+93343	Pápai Lajos
+93350	Csepelyi Gusztáv
+93351	Molnár Géza
+93353	Pálovics Csaba
+93354	Papp János
+93355	Hegedüs Gyula
+93360	Dongó Péter
+93362	Fekete László
+93363	Seidl Ferenc
+93364	Kovács Zoltán
+93367	Lenge Zoltán
+93370	Takács János
+93373	Simon Attila
+93374	Somogyi Gábor
+93376	Kitka Tibor
+93378	Major József
+93381	Biró Frigyes
+93383	Akucs László
+93384	Szalóczi János
+93387	Laczkó József
+93389	Somogyi Attila
+93391	Kovács Kálmán
+93392	Orosz László
+93394	Bozsányi Imre
+93395	Scheidt Béla
+93397	Németh Attila
+93404	Kövér Sándor
+93405	Vas Attila
+93406	Babák László
+93409	Boda József
+93411	Illés Ferenc
+93414	Takács Károly
+93415	Kocsis Zoltán
+93417	Szernecz Gábor
+93418	Érdi János
+93419	Kanyó Sándor
+93425	Zanker László
+93426	Wild Zoltán
+93429	Tóth Ferenc
+93432	Halász Géza
+93434	Kelemen János
+93435	Pap Attila
+93437	Cserny Csaba István
+93438	Füzi Attila
+93446	Lezsák Zsolt
+93452	Botyánszki Zsolt
+93453	Maka Károly
+93460	Oszkó Imre István
+93558	Bősze Katalin
+93560	Bognár Gyuláné
+93561	Jenei Lászlóné
+93566	Székely Brigitta
+93570	Szabóné Vörös Angelika
+93571	Thurzóné Fehér Hajnalka
+93575	Mártonné Vasvári Ilona
+93585	Boros Tibor István
+93608	Varga János
+93610	Nagy László
+93612	Borbás János
+93615	Székelyi István
+93616	Fülöp Mihály
+93618	Szakács Sándor
+93619	Vizi István
+93621	Jenei László
+93622	Hajdu Tibor
+93625	Ozsváth Artúr
+93627	Szalkai László
+93638	Grózinger Sándor
+93642	Bischoff László
+93742	Bakó Ignác
+93743	Gulyás Zoltán
+93747	Iván István
+93749	Plavecz József
+93750	Lojdl József Imre
+93751	Máj Tamás
+93758	Galló György
+93774	Geresics Zoltán
+93782	Szikora György
+93785	Sziládi Csaba József
+93786	Máj Henrik
+93790	Ács Attila
+93795	Vedrédi Attila
+93804	Orbán Ferenc
+93824	Fodor Tibor
+93826	Toman Nándor
+93835	Rontó Gyula
+93840	Matusik Ferenc
+93852	Bujáki János
+93855	Kovács Miklós
+93859	Haludka László
+93861	Dudás Pál
+93865	Szentmiklósi Zsolt
+93872	Takáts Géza
+93876	Palásti Zoltán
+93881	Edőcs Attila
+93897	Valkó László
+93898	Oláh Tamás
+93901	Sepler Ádám
+93914	Paunoch Krisztián
+93915	Farkas András
+93920	Gulyás Tibor
+93926	Bóna Gábor
+93941	László Tibor
+93970	Lukács Sándor
+93972	Molnár Attila
+93979	Pisiák Ágoston
+93995	Varsányi László
+94002	Kuti István
+94015	Bognár Gyula
+94026	Dóra István
+94033	Németi László Imre
+94065	Veszelik Pál András
+94130	Sipos Attila
+94135	Rettegi Tibor
+94148	Dobos József
+94153	Kaszás Ferenc
+94196	Holló János
+94209	Székely Mihály
+94232	Gémesi János
+94243	Bőr Gábor
+94244	Kuknyó László
+94288	Barát Imre
+94295	Domonyi Ferenc
+94306	Kallós Endréné
+94311	Kovács Erika
+94319	Mohos Pál
+94331	Gulyásné Sasvári Mária Erzsébet
+94336	Tápler János
+94343	Varga Gáborné
+94361	Ombodi József
+94366	Bánki Gábor
+94367	Juhász Gyula
+94378	Hegedűs Balázs
+94393	Sóvágó Zoltán
+94403	Lakatos István
+94405	Horváth Ferenc
+94410	Erdős Imre
+94425	Fundukidisz Szokrátész
+94519	Markovics Györgyné
+94528	Török Péter
+94534	Krénn Lőrinc
+94541	Galambosné Stuhán Beáta
+94573	Lovistyék János
+94578	Menyhárt Imre
+94579	Berczi Balázs
+94582	Kovács Attila
+94588	Balogh István
+94606	Szegedi Sándor
+94614	Fischer Péterné
+94627	Simon Károly
+94629	Kohári István
+94636	Patai Sándor
+94639	Kovács József László
+94646	Telek Csaba
+94650	Gyarmati Boldizsár
+94664	Papp Imre László
+94672	Jónás Gyula
+94673	Kiss András
+94676	Varga János Tamás
+94679	Balogh István
+94692	Gyulavári Gyula
+94710	Boós Tamás
+94716	Szikora Györgyné
+94717	Csik Ferenc
+94722	Seres Péter
+94723	Jabronka Sándor
+94728	Szabó János
+94733	Tóth Miklós
+94735	Sebestyén Edit
+94736	Nagy Szilveszter
+94758	Babai Csilla
+94762	Kecskeméti Boglárka
+94764	Hrazdil Krisztián
+94765	Torma Gábor
+94768	Marton Lajos
+94772	Bartal Ferenc
+94780	Varga Ferenc
+94805	Héni Tibor
+94810	Holecskó József
+94816	Németh Zoltán
+94830	Egyed István
+94831	Csizmadia Péter
+94832	Bánkiné Békés Andrea
+94834	Kelemen Éva
+94841	Dudar György
+94842	Kőröshegyi Csaba
+94864	Sántits Zoltán
+94867	Dolmány Sándorné
+94870	Plavecz András
+94877	Botos Katalin
+94880	Kiss Lajos
+94886	Kovács Róbert
+94887	Sáros György
+94892	Németh Eszter
+94896	Tokai Károly
+94897	Steer János
+94899	Kis László Barnabás
+94918	Bognár József Ferenc
+94919	Trepák István
+94925	Kallós Dóra
+94935	Kiss Ákos
+94941	Bacskai Bence
+94942	Körmendi Bence
+94949	Halák Richárd
+94952	Kis Sándor Károly
+94954	Balla Ádám
+94957	Gyetván Ádám
+94959	Monori Hajnalka
+94960	Oláh Tibor
+94964	Balla Ágnes
+94967	Kun Szabolcs
+94970	Jobban Zoltán Tibor
+94971	Sinkó Klaudia
+94973	Végh Benjámin Ákos
+94974	Murár Tamás
+94975	Mohlné Forintos Bettina
+94978	Gál József
+94981	Kaszás Tamás
+94982	Breza Arnold
+94983	Antal Krisztián
+94984	Lengyel László
+94986	Tóth Gábor
+94988	Frideczki Olivér
+94989	Sasvári Krisztina Judit
+94990	Aldrich Karin
+95001	Széles Zoltán Attila
+95009	Mezei János Imre
+95010	Ibrahem Sally
+95011	Katona Tamás
+95015	Tóth Julianna
+95020	Várszegi Gyula Ervin
+95024	Vincze Ákos
+95033	Sátori Dániel József
+95037	Lantos Krisztián
+95038	Csécs Erik
+95039	Wolfgéher Ádám
+95040	Sarkadi Sándor
+95042	Kanyó Zoltán
+95047	Kis Csaba István
+95048	Remete József
+95051	Kalmár Lajosné
+95052	Kovács Csaba
+95053	Bodnár Dániel
+95054	Eppel Károly István
+95055	Szőke Szabolcs
+95056	Molnár Csaba
+95057	Farkas Ferenc
+95058	Molnár András
+95063	Fodor Zoltán
+95067	Bán Ferenc
+95068	Kardos Gábor
+95069	Tánczos Károly
+95070	Monfera Tibor
+95075	Varga Tamás János
+95077	Remmer Tibor
+95082	Kalina Zoltán István
+95085	Antal Ferenc Nándorné
+95086	Bartók György
+95087	Koczkás János
+95088	Varga László Levente
+95092	Vágányik Mihály
+95093	Nagy László
+95094	Regényi Júlia
+95095	Kozma László
+95096	Pósfai András
+95098	Mitiner Ferenc
+95101	Szabó Csaba
+95108	Csák Dániel
+95109	Drevenka László
+95110	Góg Zsolt
+95111	Horváth Zoltán
+95112	Krénusz Bence
+95113	Kutasi Dániel
+95114	Marsi Melinda  Zsanett
+95115	Menyhárt László
+95116	Pintér Bálint
+95117	Széles Sándor
+95118	Boda Attila
+95119	Buzsik Tibor
+95120	Kohut Barbara
+\.
