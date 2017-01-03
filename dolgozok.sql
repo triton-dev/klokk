7fc0853849fee1265a28b316b368b58b8d5cebec
triton-dev <szikoragy@gmail.com>
2017-01-03 13:06:56 +0100

    Update dolgozok.sql

--- dolgozok.sql
+++ dolgozok.sql
@@ -1,314 +1,314 @@
 --
 -- SQL loader dolgozók táblához
 --
-\copy dolgozo(torzsszam, nev) from stdin
-93264	Szokolai Ferenc
-93267	Varnyu Gábor
-93268	Rajczi Gyula
-93269	Tamási Tibor
-93271	Marosán Mihály
-93272	Tóth Zoltán Károly
-93281	Szöllősi Tibor
-93284	Kele Károly
-93285	Bádi István
-93290	Katona László
-93291	Holló János
-93292	Buczynski Kazimierz Edward
-93293	Sebestyén Tibor
-93295	Csaja József
-93299	Kisvári Vendel
-93302	Várkonyi István
-93303	Erőss Mihály
-93304	Nagy István
-93306	Mityka György
-93308	Németh Béla
-93310	Varga Zoltán
-93312	Jakabovics Imre
-93313	Gellény Ottó
-93314	Csizmadia Zsigmond
-93318	Örkényi Zsolt
-93319	Kalmár István
-93330	Kis Zoltán Károly
-93338	Szabó János  Sándor
-93340	Barabás Lajos
-93343	Pápai Lajos
-93350	Csepelyi Gusztáv
-93351	Molnár Géza
-93353	Pálovics Csaba
-93354	Papp János
-93355	Hegedüs Gyula
-93360	Dongó Péter
-93362	Fekete László
-93363	Seidl Ferenc
-93364	Kovács Zoltán
-93367	Lenge Zoltán
-93370	Takács János
-93373	Simon Attila
-93374	Somogyi Gábor
-93376	Kitka Tibor
-93378	Major József
-93381	Biró Frigyes
-93383	Akucs László
-93384	Szalóczi János
-93387	Laczkó József
-93389	Somogyi Attila
-93391	Kovács Kálmán
-93392	Orosz László
-93394	Bozsányi Imre
-93395	Scheidt Béla
-93397	Németh Attila
-93404	Kövér Sándor
-93405	Vas Attila
-93406	Babák László
-93409	Boda József
-93411	Illés Ferenc
-93414	Takács Károly
-93415	Kocsis Zoltán
-93417	Szernecz Gábor
-93418	Érdi János
-93419	Kanyó Sándor
-93425	Zanker László
-93426	Wild Zoltán
-93429	Tóth Ferenc
-93432	Halász Géza
-93434	Kelemen János
-93435	Pap Attila
-93437	Cserny Csaba István
-93438	Füzi Attila
-93446	Lezsák Zsolt
-93452	Botyánszki Zsolt
-93453	Maka Károly
-93460	Oszkó Imre István
-93558	Bősze Katalin
-93560	Bognár Gyuláné
-93561	Jenei Lászlóné
-93566	Székely Brigitta
-93570	Szabóné Vörös Angelika
-93571	Thurzóné Fehér Hajnalka
-93575	Mártonné Vasvári Ilona
-93585	Boros Tibor István
-93608	Varga János
-93610	Nagy László
-93612	Borbás János
-93615	Székelyi István
-93616	Fülöp Mihály
-93618	Szakács Sándor
-93619	Vizi István
-93621	Jenei László
-93622	Hajdu Tibor
-93625	Ozsváth Artúr
-93627	Szalkai László
-93638	Grózinger Sándor
-93642	Bischoff László
-93742	Bakó Ignác
-93743	Gulyás Zoltán
-93747	Iván István
-93749	Plavecz József
-93750	Lojdl József Imre
-93751	Máj Tamás
-93758	Galló György
-93774	Geresics Zoltán
-93782	Szikora György
-93785	Sziládi Csaba József
-93786	Máj Henrik
-93790	Ács Attila
-93795	Vedrédi Attila
-93804	Orbán Ferenc
-93824	Fodor Tibor
-93826	Toman Nándor
-93835	Rontó Gyula
-93840	Matusik Ferenc
-93852	Bujáki János
-93855	Kovács Miklós
-93859	Haludka László
-93861	Dudás Pál
-93865	Szentmiklósi Zsolt
-93872	Takáts Géza
-93876	Palásti Zoltán
-93881	Edőcs Attila
-93897	Valkó László
-93898	Oláh Tamás
-93901	Sepler Ádám
-93914	Paunoch Krisztián
-93915	Farkas András
-93920	Gulyás Tibor
-93926	Bóna Gábor
-93941	László Tibor
-93970	Lukács Sándor
-93972	Molnár Attila
-93979	Pisiák Ágoston
-93995	Varsányi László
-94002	Kuti István
-94015	Bognár Gyula
-94026	Dóra István
-94033	Németi László Imre
-94065	Veszelik Pál András
-94130	Sipos Attila
-94135	Rettegi Tibor
-94148	Dobos József
-94153	Kaszás Ferenc
-94196	Holló János
-94209	Székely Mihály
-94232	Gémesi János
-94243	Bőr Gábor
-94244	Kuknyó László
-94288	Barát Imre
-94295	Domonyi Ferenc
-94306	Kallós Endréné
-94311	Kovács Erika
-94319	Mohos Pál
-94331	Gulyásné Sasvári Mária Erzsébet
-94336	Tápler János
-94343	Varga Gáborné
-94361	Ombodi József
-94366	Bánki Gábor
-94367	Juhász Gyula
-94378	Hegedűs Balázs
-94393	Sóvágó Zoltán
-94403	Lakatos István
-94405	Horváth Ferenc
-94410	Erdős Imre
-94425	Fundukidisz Szokrátész
-94519	Markovics Györgyné
-94528	Török Péter
-94534	Krénn Lőrinc
-94541	Galambosné Stuhán Beáta
-94573	Lovistyék János
-94578	Menyhárt Imre
-94579	Berczi Balázs
-94582	Kovács Attila
-94588	Balogh István
-94606	Szegedi Sándor
-94614	Fischer Péterné
-94627	Simon Károly
-94629	Kohári István
-94636	Patai Sándor
-94639	Kovács József László
-94646	Telek Csaba
-94650	Gyarmati Boldizsár
-94664	Papp Imre László
-94672	Jónás Gyula
-94673	Kiss András
-94676	Varga János Tamás
-94679	Balogh István
-94692	Gyulavári Gyula
-94710	Boós Tamás
-94716	Szikora Györgyné
-94717	Csik Ferenc
-94722	Seres Péter
-94723	Jabronka Sándor
-94728	Szabó János
-94733	Tóth Miklós
-94735	Sebestyén Edit
-94736	Nagy Szilveszter
-94758	Babai Csilla
-94762	Kecskeméti Boglárka
-94764	Hrazdil Krisztián
-94765	Torma Gábor
-94768	Marton Lajos
-94772	Bartal Ferenc
-94780	Varga Ferenc
-94805	Héni Tibor
-94810	Holecskó József
-94816	Németh Zoltán
-94830	Egyed István
-94831	Csizmadia Péter
-94832	Bánkiné Békés Andrea
-94834	Kelemen Éva
-94841	Dudar György
-94842	Kőröshegyi Csaba
-94864	Sántits Zoltán
-94867	Dolmány Sándorné
-94870	Plavecz András
-94877	Botos Katalin
-94880	Kiss Lajos
-94886	Kovács Róbert
-94887	Sáros György
-94892	Németh Eszter
-94896	Tokai Károly
-94897	Steer János
-94899	Kis László Barnabás
-94918	Bognár József Ferenc
-94919	Trepák István
-94925	Kallós Dóra
-94935	Kiss Ákos
-94941	Bacskai Bence
-94942	Körmendi Bence
-94949	Halák Richárd
-94952	Kis Sándor Károly
-94954	Balla Ádám
-94957	Gyetván Ádám
-94959	Monori Hajnalka
-94960	Oláh Tibor
-94964	Balla Ágnes
-94967	Kun Szabolcs
-94970	Jobban Zoltán Tibor
-94971	Sinkó Klaudia
-94973	Végh Benjámin Ákos
-94974	Murár Tamás
-94975	Mohlné Forintos Bettina
-94978	Gál József
-94981	Kaszás Tamás
-94982	Breza Arnold
-94983	Antal Krisztián
-94984	Lengyel László
-94986	Tóth Gábor
-94988	Frideczki Olivér
-94989	Sasvári Krisztina Judit
-94990	Aldrich Karin
-95001	Széles Zoltán Attila
-95009	Mezei János Imre
-95010	Ibrahem Sally
-95011	Katona Tamás
-95015	Tóth Julianna
-95020	Várszegi Gyula Ervin
-95024	Vincze Ákos
-95033	Sátori Dániel József
-95037	Lantos Krisztián
-95038	Csécs Erik
-95039	Wolfgéher Ádám
-95040	Sarkadi Sándor
-95042	Kanyó Zoltán
-95047	Kis Csaba István
-95048	Remete József
-95051	Kalmár Lajosné
-95052	Kovács Csaba
-95053	Bodnár Dániel
-95054	Eppel Károly István
-95055	Szőke Szabolcs
-95056	Molnár Csaba
-95057	Farkas Ferenc
-95058	Molnár András
-95063	Fodor Zoltán
-95067	Bán Ferenc
-95068	Kardos Gábor
-95069	Tánczos Károly
-95070	Monfera Tibor
-95075	Varga Tamás János
-95077	Remmer Tibor
-95082	Kalina Zoltán István
-95085	Antal Ferenc Nándorné
-95086	Bartók György
-95087	Koczkás János
-95088	Varga László Levente
-95092	Vágányik Mihály
-95093	Nagy László
-95094	Regényi Júlia
-95095	Kozma László
-95096	Pósfai András
-95098	Mitiner Ferenc
-95101	Szabó Csaba
-95108	Csák Dániel
-95109	Drevenka László
-95110	Góg Zsolt
-95111	Horváth Zoltán
-95112	Krénusz Bence
-95113	Kutasi Dániel
-95114	Marsi Melinda  Zsanett
-95115	Menyhárt László
-95116	Pintér Bálint
-95117	Széles Sándor
-95118	Boda Attila
-95119	Buzsik Tibor
-95120	Kohut Barbara
+\copy dolgozo from stdin
+93585	Boros Tibor István	11-111-0	nopic.jpg
+93360	Dongó Péter	11-110-0	nopic.jpg
+95051	Kalmár Lajosné	11-110-0	nopic.jpg
+94892	Németh Eszter	11-110-0	nopic.jpg
+94780	Varga Ferenc	11-110-0	nopic.jpg
+95020	Várszegi Gyula Ervin	11-110-0	nopic.jpg
+95010	Ibrahem Sally	11-335-0	nopic.jpg
+94970	Jobban Zoltán Tibor	11-335-0	nopic.jpg
+93394	Bozsányi Imre	14-460-0	nopic.jpg
+93743	Gulyás Zoltán	14-460-0	nopic.jpg
+94805	Héni Tibor	14-460-0	nopic.jpg
+94989	Sasvári Krisztina Judit	14-460-0	nopic.jpg
+93293	Sebestyén Tibor	14-460-0	nopic.jpg
+94816	Németh Zoltán	11-200-0	nopic.jpg
+94877	Botos Katalin	11-210-0	nopic.jpg
+94834	Kelemen Éva	11-210-0	nopic.jpg
+94975	Mohlné Forintos Bettina	11-210-0	nopic.jpg
+94954	Balla Ádám	11-220-0	nopic.jpg
+94832	Bánkiné Békés Andrea	11-220-0	nopic.jpg
+94331	Gulyásné Sasvári Mária Erzsébet	11-220-0	nopic.jpg
+94925	Kallós Dóra	11-220-0	nopic.jpg
+94306	Kallós Endréné	11-230-0	nopic.jpg
+94519	Markovics Györgyné	11-230-0	nopic.jpg
+94971	Sinkó Klaudia	11-230-0	nopic.jpg
+95015	Tóth Julianna	11-230-0	nopic.jpg
+94343	Varga Gáborné	11-230-0	nopic.jpg
+94964	Balla Ágnes	11-250-0	nopic.jpg
+95094	Regényi Júlia	11-250-0	nopic.jpg
+93920	Gulyás Tibor	11-260-0	nopic.jpg
+94957	Gyetván Ádám	11-260-0	nopic.jpg
+93621	Jenei László	11-260-0	nopic.jpg
+94942	Körmendi Bence	11-260-0	nopic.jpg
+94959	Monori Hajnalka	11-260-0	nopic.jpg
+94735	Sebestyén Edit	11-260-0	nopic.jpg
+93409	Boda József	21-322-1	nopic.jpg
+93312	Jakabovics Imre	21-322-1	nopic.jpg
+94880	Kiss Lajos	21-322-1	nopic.jpg
+94716	Szikora Györgyné	21-322-1	nopic.jpg
+95085	Antal Ferenc Nándorné	21-322-3	nopic.jpg
+95114	Marsi Melinda  Zsanett	21-322-3	nopic.jpg
+93292	Buczynski Kazimierz Edward	11-400-0	nopic.jpg
+94579	Berczi Balázs	12-420-0	nopic.jpg
+93418	Érdi János	12-420-0	nopic.jpg
+93758	Galló György	12-420-0	nopic.jpg
+95095	Kozma László	12-420-0	nopic.jpg
+93575	Mártonné Vasvári Ilona	12-420-0	nopic.jpg
+95093	Nagy László	12-420-0	nopic.jpg
+93395	Scheidt Béla	12-420-0	nopic.jpg
+93310	Varga Zoltán	12-420-0	nopic.jpg
+93340	Barabás Lajos	12-430-0	nopic.jpg
+93438	Füzi Attila	12-430-0	nopic.jpg
+93353	Pálovics Csaba	12-430-0	nopic.jpg
+93425	Zanker László	12-430-0	nopic.jpg
+94983	Antal Krisztián	13-421-0	nopic.jpg
+94941	Bacskai Bence	13-421-0	nopic.jpg
+95038	Csécs Erik	13-421-0	nopic.jpg
+93774	Geresics Zoltán	13-421-0	nopic.jpg
+94764	Hrazdil Krisztián	13-421-0	nopic.jpg
+93411	Illés Ferenc	13-421-0	nopic.jpg
+94723	Jabronka Sándor	13-421-0	nopic.jpg
+95068	Kardos Gábor	13-421-0	nopic.jpg
+93284	Kele Károly	13-421-0	nopic.jpg
+94573	Lovistyék János	13-421-0	nopic.jpg
+94578	Menyhárt Imre	13-421-0	nopic.jpg
+93972	Molnár Attila	13-421-0	nopic.jpg
+95056	Molnár Csaba	13-421-0	nopic.jpg
+93898	Oláh Tamás	13-421-0	nopic.jpg
+93435	Pap Attila	13-421-0	nopic.jpg
+95096	Pósfai András	13-421-0	nopic.jpg
+94728	Szabó János	13-421-0	nopic.jpg
+95001	Széles Zoltán Attila	13-421-0	nopic.jpg
+93370	Takács János	13-421-0	nopic.jpg
+95069	Tánczos Károly	13-421-0	nopic.jpg
+94765	Torma Gábor	13-421-0	nopic.jpg
+94733	Tóth Miklós	13-421-0	nopic.jpg
+95088	Varga László Levente	13-421-0	nopic.jpg
+93405	Vas Attila	13-421-0	nopic.jpg
+93406	Babák László	13-422-0	nopic.jpg
+94772	Bartal Ferenc	13-422-0	nopic.jpg
+95119	Buzsik Tibor	13-422-0	nopic.jpg
+94148	Dobos József	13-422-0	nopic.jpg
+94026	Dóra István	13-422-0	nopic.jpg
+95063	Fodor Zoltán	13-422-0	nopic.jpg
+94425	Fundukidisz Szokrátész	13-422-0	nopic.jpg
+93638	Grózinger Sándor	13-422-0	nopic.jpg
+94378	Hegedűs Balázs	13-422-0	nopic.jpg
+93376	Kitka Tibor	13-422-0	nopic.jpg
+93415	Kocsis Zoltán	13-422-0	nopic.jpg
+94002	Kuti István	13-422-0	nopic.jpg
+94984	Lengyel László	13-422-0	nopic.jpg
+93786	Máj Henrik	13-422-0	nopic.jpg
+93453	Maka Károly	13-422-0	nopic.jpg
+95070	Monfera Tibor	13-422-0	nopic.jpg
+93625	Ozsváth Artúr	13-422-0	nopic.jpg
+93914	Paunoch Krisztián	13-422-0	nopic.jpg
+94722	Seres Péter	13-422-0	nopic.jpg
+93865	Szentmiklósi Zsolt	13-422-0	nopic.jpg
+93790	Ács Attila	13-423-0	nopic.jpg
+95067	Bán Ferenc	13-423-0	nopic.jpg
+95086	Bartók György	13-423-0	nopic.jpg
+93642	Bischoff László	13-423-0	nopic.jpg
+93350	Csepelyi Gusztáv	13-423-0	nopic.jpg
+94295	Domonyi Ferenc	13-423-0	nopic.jpg
+94988	Frideczki Olivér	13-423-0	nopic.jpg
+93432	Halász Géza	13-423-0	nopic.jpg
+95082	Kalina Zoltán István	13-423-0	nopic.jpg
+93419	Kanyó Sándor	13-423-0	nopic.jpg
+94899	Kis László Barnabás	13-423-0	nopic.jpg
+93330	Kis Zoltán Károly	13-423-0	nopic.jpg
+93299	Kisvári Vendel	13-423-0	nopic.jpg
+94534	Krénn Lőrinc	13-423-0	nopic.jpg
+93367	Lenge Zoltán	13-423-0	nopic.jpg
+93751	Máj Tamás	13-423-0	nopic.jpg
+93351	Molnár Géza	13-423-0	nopic.jpg
+93392	Orosz László	13-423-0	nopic.jpg
+93373	Simon Attila	13-423-0	nopic.jpg
+94393	Sóvágó Zoltán	13-423-0	nopic.jpg
+93417	Szernecz Gábor	13-423-0	nopic.jpg
+94336	Tápler János	13-423-0	nopic.jpg
+93429	Tóth Ferenc	13-423-0	nopic.jpg
+94528	Török Péter	13-423-0	nopic.jpg
+95075	Varga Tamás János	13-423-0	nopic.jpg
+93383	Akucs László	13-424-0	nopic.jpg
+94588	Balogh István	13-424-0	nopic.jpg
+95118	Boda Attila	13-424-0	nopic.jpg
+94841	Dudar György	13-424-0	nopic.jpg
+94692	Gyulavári Gyula	13-424-0	nopic.jpg
+93859	Haludka László	13-424-0	nopic.jpg
+94629	Kohári István	13-424-0	nopic.jpg
+94582	Kovács Attila	13-424-0	nopic.jpg
+94639	Kovács József László	13-424-0	nopic.jpg
+94842	Kőröshegyi Csaba	13-424-0	nopic.jpg
+95113	Kutasi Dániel	13-424-0	nopic.jpg
+93840	Matusik Ferenc	13-424-0	nopic.jpg
+94960	Oláh Tibor	13-424-0	nopic.jpg
+95116	Pintér Bálint	13-424-0	nopic.jpg
+95033	Sátori Dániel József	13-424-0	nopic.jpg
+93414	Takács Károly	13-424-0	nopic.jpg
+93285	Bádi István	13-425-0	nopic.jpg
+94366	Bánki Gábor	13-425-0	nopic.jpg
+94243	Bőr Gábor	13-425-0	nopic.jpg
+94232	Gémesi János	13-425-0	nopic.jpg
+94244	Kuknyó László	13-425-0	nopic.jpg
+93271	Marosán Mihály	13-425-0	nopic.jpg
+93876	Palásti Zoltán	13-425-0	nopic.jpg
+94209	Székely Mihály	13-425-0	nopic.jpg
+95117	Széles Sándor	13-425-0	nopic.jpg
+93264	Szokolai Ferenc	13-425-0	nopic.jpg
+93269	Tamási Tibor	13-425-0	nopic.jpg
+95092	Vágányik Mihály	13-425-0	nopic.jpg
+93995	Varsányi László	13-425-0	nopic.jpg
+93381	Biró Frigyes	13-431-0	nopic.jpg
+93295	Csaja József	13-431-0	nopic.jpg
+93314	Csizmadia Zsigmond	13-431-0	nopic.jpg
+94410	Erdős Imre	13-431-0	nopic.jpg
+93561	Jenei Lászlóné	13-431-0	nopic.jpg
+95098	Mitiner Ferenc	13-431-0	nopic.jpg
+94319	Mohos Pál	13-431-0	nopic.jpg
+94135	Rettegi Tibor	13-431-0	nopic.jpg
+94864	Sántits Zoltán	13-431-0	nopic.jpg
+95040	Sarkadi Sándor	13-431-0	nopic.jpg
+95101	Szabó Csaba	13-431-0	nopic.jpg
+93872	Takáts Géza	13-431-0	nopic.jpg
+93267	Varnyu Gábor	13-431-0	nopic.jpg
+94065	Veszelik Pál András	13-431-0	nopic.jpg
+93452	Botyánszki Zsolt	13-432-0	nopic.jpg
+93852	Bujáki János	13-432-0	nopic.jpg
+95057	Farkas Ferenc	13-432-0	nopic.jpg
+93824	Fodor Tibor	13-432-0	nopic.jpg
+93355	Hegedüs Gyula	13-432-0	nopic.jpg
+94810	Holecskó József	13-432-0	nopic.jpg
+94405	Horváth Ferenc	13-432-0	nopic.jpg
+95047	Kis Csaba István	13-432-0	nopic.jpg
+94403	Lakatos István	13-432-0	nopic.jpg
+93446	Lezsák Zsolt	13-432-0	nopic.jpg
+93750	Lojdl József Imre	13-432-0	nopic.jpg
+93378	Major József	13-432-0	nopic.jpg
+93304	Nagy István	13-432-0	nopic.jpg
+93804	Orbán Ferenc	13-432-0	nopic.jpg
+94870	Plavecz András	13-432-0	nopic.jpg
+93363	Seidl Ferenc	13-432-0	nopic.jpg
+94606	Szegedi Sándor	13-432-0	nopic.jpg
+93785	Sziládi Csaba József	13-432-0	nopic.jpg
+95055	Szőke Szabolcs	13-432-0	nopic.jpg
+94676	Varga János Tamás	13-432-0	nopic.jpg
+93742	Bakó Ignác	13-433-0	nopic.jpg
+94679	Balogh István	13-433-0	nopic.jpg
+93881	Edőcs Attila	13-433-0	nopic.jpg
+94949	Halák Richárd	13-433-0	nopic.jpg
+94367	Juhász Gyula	13-433-0	nopic.jpg
+94153	Kaszás Ferenc	13-433-0	nopic.jpg
+95087	Koczkás János	13-433-0	nopic.jpg
+93391	Kovács Kálmán	13-433-0	nopic.jpg
+93364	Kovács Zoltán	13-433-0	nopic.jpg
+93941	László Tibor	13-433-0	nopic.jpg
+93970	Lukács Sándor	13-433-0	nopic.jpg
+95115	Menyhárt László	13-433-0	nopic.jpg
+93460	Oszkó Imre István	13-433-0	nopic.jpg
+93835	Rontó Gyula	13-433-0	nopic.jpg
+93384	Szalóczi János	13-433-0	nopic.jpg
+94646	Telek Csaba	13-433-0	nopic.jpg
+93795	Vedrédi Attila	13-433-0	nopic.jpg
+94918	Bognár József Ferenc	13-441-0	nopic.jpg
+95109	Drevenka László	13-441-0	nopic.jpg
+93861	Dudás Pál	13-441-0	nopic.jpg
+93313	Gellény Ottó	13-441-0	nopic.jpg
+95111	Horváth Zoltán	13-441-0	nopic.jpg
+94672	Jónás Gyula	13-441-0	nopic.jpg
+95042	Kanyó Zoltán	13-441-0	nopic.jpg
+93290	Katona László	13-441-0	nopic.jpg
+94886	Kovács Róbert	13-441-0	nopic.jpg
+94768	Marton Lajos	13-441-0	nopic.jpg
+94736	Nagy Szilveszter	13-441-0	nopic.jpg
+93308	Németh Béla	13-441-0	nopic.jpg
+93343	Pápai Lajos	13-441-0	nopic.jpg
+94887	Sáros György	13-441-0	nopic.jpg
+94897	Steer János	13-441-0	nopic.jpg
+93338	Szabó János  Sándor	13-441-0	nopic.jpg
+93826	Toman Nándor	13-441-0	nopic.jpg
+93272	Tóth Zoltán Károly	13-441-0	nopic.jpg
+94919	Trepák István	13-441-0	nopic.jpg
+93302	Várkonyi István	13-441-0	nopic.jpg
+95053	Bodnár Dániel	13-443-0	nopic.jpg
+93926	Bóna Gábor	13-443-0	nopic.jpg
+93437	Cserny Csaba István	13-443-0	nopic.jpg
+93616	Fülöp Mihály	13-443-0	nopic.jpg
+93319	Kalmár István	13-443-0	nopic.jpg
+94981	Kaszás Tamás	13-443-0	nopic.jpg
+94935	Kiss Ákos	13-443-0	nopic.jpg
+95052	Kovács Csaba	13-443-0	nopic.jpg
+95037	Lantos Krisztián	13-443-0	nopic.jpg
+94033	Németi László Imre	13-443-0	nopic.jpg
+94361	Ombodi József	13-443-0	nopic.jpg
+93354	Papp János	13-443-0	nopic.jpg
+93979	Pisiák Ágoston	13-443-0	nopic.jpg
+93426	Wild Zoltán	13-443-0	nopic.jpg
+94982	Breza Arnold	13-444-0	nopic.jpg
+93622	Hajdu Tibor	13-444-0	nopic.jpg
+93610	Nagy László	13-444-0	nopic.jpg
+93619	Vizi István	13-444-0	nopic.jpg
+93915	Farkas András	13-501-0	nopic.jpg
+95011	Katona Tamás	13-501-0	nopic.jpg
+94967	Kun Szabolcs	13-501-0	nopic.jpg
+94130	Sipos Attila	13-501-0	nopic.jpg
+94896	Tokai Károly	13-501-0	nopic.jpg
+93558	Bősze Katalin	13-503-0	nopic.jpg
+93747	Iván István	13-503-0	nopic.jpg
+93434	Kelemen János	13-503-0	nopic.jpg
+94311	Kovács Erika	13-503-0	nopic.jpg
+93404	Kövér Sándor	13-503-0	nopic.jpg
+93374	Somogyi Gábor	13-503-0	nopic.jpg
+93570	Szabóné Vörös Angelika	13-503-0	nopic.jpg
+93566	Székely Brigitta	13-503-0	nopic.jpg
+93782	Szikora György	13-503-0	nopic.jpg
+93571	Thurzóné Fehér Hajnalka	13-503-0	nopic.jpg
+93897	Valkó László	13-503-0	nopic.jpg
+94196	Holló János	11-120-0	nopic.jpg
+93901	Sepler Ádám	11-120-0	nopic.jpg
+94762	Kecskeméti Boglárka	11-300-0	nopic.jpg
+93281	Szöllősi Tibor	11-300-0	nopic.jpg
+94758	Babai Csilla	11-310-0	nopic.jpg
+94015	Bognár Gyula	11-310-0	nopic.jpg
+93560	Bognár Gyuláné	11-310-0	nopic.jpg
+94831	Csizmadia Péter	11-310-0	nopic.jpg
+93303	Erőss Mihály	11-310-0	nopic.jpg
+93362	Fekete László	11-310-0	nopic.jpg
+93291	Holló János	11-310-0	nopic.jpg
+94673	Kiss András	11-310-0	nopic.jpg
+93855	Kovács Miklós	11-310-0	nopic.jpg
+93387	Laczkó József	11-310-0	nopic.jpg
+93306	Mityka György	11-310-0	nopic.jpg
+93318	Örkényi Zsolt	11-310-0	nopic.jpg
+94627	Simon Károly	11-310-0	nopic.jpg
+93389	Somogyi Attila	11-310-0	nopic.jpg
+94973	Végh Benjámin Ákos	11-310-0	nopic.jpg
+94288	Barát Imre	11-330-0	nopic.jpg
+95108	Csák Dániel	11-330-0	nopic.jpg
+94830	Egyed István	11-330-0	nopic.jpg
+94614	Fischer Péterné	11-330-0	nopic.jpg
+94978	Gál József	11-330-0	nopic.jpg
+95058	Molnár András	11-330-0	nopic.jpg
+93397	Németh Attila	11-330-0	nopic.jpg
+93618	Szakács Sándor	11-330-0	nopic.jpg
+95024	Vincze Ákos	11-330-0	nopic.jpg
+94990	Aldrich Karin	11-410-0	nopic.jpg
+94867	Dolmány Sándorné	11-410-0	nopic.jpg
+94541	Galambosné Stuhán Beáta	11-410-0	nopic.jpg
+95120	Kohut Barbara	11-410-0	nopic.jpg
+95009	Mezei János Imre	11-410-0	nopic.jpg
+94974	Murár Tamás	11-410-0	nopic.jpg
+94986	Tóth Gábor	11-410-0	nopic.jpg
+94710	Boós Tamás	11-411-0	nopic.jpg
+95112	Krénusz Bence	11-411-0	nopic.jpg
+94636	Patai Sándor	11-411-0	nopic.jpg
+95077	Remmer Tibor	11-411-0	nopic.jpg
+94717	Csik Ferenc	13-140-0	nopic.jpg
+94650	Gyarmati Boldizsár	13-140-0	nopic.jpg
+94952	Kis Sándor Károly	13-140-0	nopic.jpg
+93749	Plavecz József	13-140-0	nopic.jpg
+93612	Borbás János	13-333-0	nopic.jpg
+95054	Eppel Károly István	13-333-0	nopic.jpg
+95110	Góg Zsolt	13-333-0	nopic.jpg
+94664	Papp Imre László	13-333-0	nopic.jpg
+93268	Rajczi Gyula	13-333-0	nopic.jpg
+95048	Remete József	13-333-0	nopic.jpg
+93627	Szalkai László	13-333-0	nopic.jpg
+93615	Székelyi István	13-333-0	nopic.jpg
+93608	Varga János	13-333-0	nopic.jpg
+95039	Wolfgéher Ádám	13-333-0	nopic.jpg
 \.
