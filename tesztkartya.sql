6ff5b21850bd88aa7ca94433bb06d6ec215e9ea8
triton-dev <szikoragy@gmail.com>
2017-01-11 10:33:15 +0100

    Create tesztkartya.sql

---
+++ tesztkartya.sql
@@ -0,0 +1,15 @@
+-- Tesztkártyák dolgozókhoz rendelése
+
+ update kartya set kartyaszam='0001123448' where torzsszam='93271';-- Marosán Mihály
+ update kartya set kartyaszam='0001063039' where torzsszam='94232';-- Gémesi János
+ update kartya set kartyaszam='0000611676' where torzsszam='95092';-- Vágányik Mihály
+ update kartya set kartyaszam='0000629357' where torzsszam='94243';-- Bőr Gábor
+ update kartya set kartyaszam='0000583022' where torzsszam='94244';-- Kuknyó László
+ update kartya set kartyaszam='0001066002' where torzsszam='93264';-- Szokolai Ferenc
+ update kartya set kartyaszam='0000608039' where torzsszam='93285';-- Bádi István
+ update kartya set kartyaszam='0000598608' where torzsszam='93995';-- Varsányi László
+ update kartya set kartyaszam='0000609815' where torzsszam='94366';-- Bánki Gábor
+ update kartya set kartyaszam='0001009344' where torzsszam='93876';-- Palásti Zoltán
+ update kartya set kartyaszam='0000000001' where torzsszam='95117';-- Széles Sándor
+ update kartya set kartyaszam='0000000002' where torzsszam='94209';-- Székely Mihály
+ update kartya set kartyaszam='0000000003' where torzsszam='93269';-- Tamási Tibor
