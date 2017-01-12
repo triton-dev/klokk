e260c163db32c7e04d9ce2b040fe514f18d752ad
triton-dev <szikoragy@gmail.com>
2017-01-12 14:22:57 +0100

    Update rendelesek.sql

--- rendelesek.sql
+++ rendelesek.sql
@@ -1,58 +1,60 @@
 -- 
 -- SQL betöltő rendelések táblához
 --
-\copy rendeles from stdin
-16334240	ÁLLÁSIDŐ	Állásidő rendelés	1	0	t
-71019154	TW6000J1AL	TW 6000 jármű J1-es alapjavítása	1	8	t
-71019156	TW6000J1AL	TW 6000 jármű J1-es alapjavítása	1	7	t
-71019157	MUZEUMI ALAP	Múzeumi jármű alapjavítása	1	0	t
-71019171	KCSV-7ÁTFAL	KCSV-7 típ. villamos átalak., felújít al	1	0	t
-71019172	KCSV-7ÁTFOP	KCSV-7 típ. villamos átalak., felújít op	1	0	t
-71019183	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	1	t
-71019185	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	2	t
-71019187	T5C5 J1 ALAP	Tátra Jármű J1-es alapjavítása	1	1	t
-71019189	T5C5 J1 ALAP	Tátra Jármű J1-es alapjavítása	1	2	t
-71019191	JÁREGYEDIAL	Különféle járművek egyedi alapjavítása	1	0	t
-71019197	JÁREGYEDIAL	Különféle járművek egyedi alapjavítása	1	0	t
-71019201	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	3	t
-71019203	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4	t
-71019208	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	3	t
-71019211	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	5	t
-71019213	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	7	t
-71019215	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	8	t
-71019217	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	6	t
-71019219	KCSV-7ÁTFAL	KCSV-7 típ. villamos átalak., felújít al	1	0	t
-71019220	KCSV-7ÁTFOP	KCSV-7 típ. villamos átalak., felújít op	1	0	t
-71019221	KCSVSÉRÜLTAL	KCSV-7 tip. jármű sérűlés alapjavítása	1	0	t
-71019223	KCSVSÉRÜLTAL	KCSV-7 tip. jármű sérűlés alapjavítása	1	0	t
-71019225	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	4	t
-71019227	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	5	t
-72047823	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	0	t
-72047824	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	0	t
-72047825	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	0	t
-73050208	9113130400-1	Lefutó szarv (Szarv) áramszedő csúszóhoz	20	0	t
-73050589	9201010044-1	Sínfék ütköző metamid lap 10x35x100	98	0	t
-73050923	9101170020-1	CSÚSZÓ DARAB	50	0	t
-73050981	9123010590-1	Homoksz.doboz rozsdam.kpl.gn.(4db)korsz.	4	0	t
-73051166	9201010040-1	Kopólemez I. forgóvázhoz TW 6000	160	0	t
-73051168	9201010100-1	Kábelbilincs garn.FV-hoz 17-féle bilincs	10	0	t
-73051175	9201181058-1	Kopólemez 10x75x80 Oliex 1200 TW 6000	20	0	t
-73051188	9201182040-1	Csúszóidom 45x40x90 TW 6000	20	0	t
-74053991	9715210000-K	Vezetőülés Combinohoz  Möve 723.22.1	20	61003455	t
-74054138	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	4125	t
-74054139	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	4125	t
-74054149	9151040000-K	KONTAKTOR SL 11/A	1	4125	t
-74054180	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	4260	t
-74054181	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	4260	t
-74054191	9151040000-K	KONTAKTOR SL 11/A	1	4260	t
-74054206	9111050010-K	URH állócsatlás 18 eres	2	4125	t
-74054207	9111050010-K	URH állócsatlás 18 eres	2	4260	t
-74054223	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	4153	t
-74054224	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	4153	t
-74054234	9151040000-K	KONTAKTOR SL 11/A	1	4153	t
-74054246	9111050010-K	URH állócsatlás 18 eres	2	4153	t
-74054250	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	4216	t
-74054251	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	4216	t
-74054261	9151040000-K	KONTAKTOR SL 11/A	1	4216	t
-74054274	9111050010-K	URH állócsatlás 18 eres	2	4216	t
+-- 
+-- SQL betöltő rendelések táblához
+--
+\copy rendeles from stdin null as '\n'
+71019154	TW6000J1AL	TW 6000 jármű J1-es alapjavítása	1	1566	8	t
+71019156	TW6000J1AL	TW 6000 jármű J1-es alapjavítása	1	1559	7	t
+71019157	MUZEUMI ALAP	Múzeumi jármű alapjavítása	1	3104	\n	t
+71019171	KCSV-7ÁTFAL	KCSV-7 típ. villamos átalak., felújít al	1	1337	\n	t
+71019172	KCSV-7ÁTFOP	KCSV-7 típ. villamos átalak., felújít op	1	1337	\n	t
+71019183	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4130	27	t
+71019185	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4229	28	t
+71019187	T5C5 J1 ALAP	Tátra Jármű J1-es alapjavítása	1	4000	1	t
+71019189	T5C5 J1 ALAP	Tátra Jármű J1-es alapjavítása	1	4171	2	t
+71019191	JÁREGYEDIAL	Különféle járművek egyedi alapjavítása	1	804	\n	t
+71019197	JÁREGYEDIAL	Különféle járművek egyedi alapjavítása	1	807	\n	t
+71019201	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4125	1	t
+71019203	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4260	2	t
+71019208	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	2003	3	t
+71019211	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4153	3	t
+71019213	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4246	5	t
+71019215	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4302	6	t
+71019217	T5C5K2MODAL	T5C5 korszer. K2 módosítás alap	1	4216	4	t
+71019219	KCSV-7ÁTFAL	KCSV-7 típ. villamos átalak., felújít al	1	1327	\n	t
+71019220	KCSV-7ÁTFOP	KCSV-7 típ. villamos átalak., felújít op	1	1327	\n	t
+71019221	KCSVSÉRÜLTAL	KCSV-7 tip. jármű sérűlés alapjavítása	1	1343	\n	t
+71019223	KCSVSÉRÜLTAL	KCSV-7 tip. jármű sérűlés alapjavítása	1	1335	\n	t
+71019225	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	2004	4	t
+71019227	COMB-FÉNYAL	Combino jármű fényezési munkák  alap	1	2005	5	t
+72047823	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	37	\n	t
+72047824	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	40	\n	t
+72047825	3391220000-2	STATIKUS ÁRAMÁTALAKITO javítása	1	43	\n	t
+73050208	9113130400-1	Lefutó szarv (Szarv) áramszedő csúszóhoz	20	\n	\n	t
+73050589	9201010044-1	Sínfék ütköző metamid lap 10x35x100	98	\n	\n	t
+73050923	9101170020-1	CSÚSZÓ DARAB	50	\n	\n	t
+73050981	9123010590-1	Homoksz.doboz rozsdam.kpl.gn.(4db)korsz.	4	\n	\n	t
+73051166	9201010040-1	Kopólemez I. forgóvázhoz TW 6000	160	\n	\n	t
+73051168	9201010100-1	Kábelbilincs garn.FV-hoz 17-féle bilincs	10	\n	\n	t
+73051175	9201181058-1	Kopólemez 10x75x80 Oliex 1200 TW 6000	20	\n	\n	t
+73051188	9201182040-1	Csúszóidom 45x40x90 TW 6000	20	\n	\n	t
+74053991	9715210000-K	Vezetőülés Combinohoz  Möve 723.22.1	20	74053991	61003455	t
+74054138	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	74054138	4125	t
+74054139	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	74054139	4125	t
+74054149	9151040000-K	KONTAKTOR SL 11/A	1	74054149	4125	t
+74054180	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	74054180	4260	t
+74054181	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	74054181	4260	t
+74054191	9151040000-K	KONTAKTOR SL 11/A	1	74054191	4260	t
+74054206	9111050010-K	URH állócsatlás 18 eres	2	74054206	4125	t
+74054207	9111050010-K	URH állócsatlás 18 eres	2	74054207	4260	t
+74054223	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	74054223	4153	t
+74054224	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	74054224	4153	t
+74054234	9151040000-K	KONTAKTOR SL 11/A	1	74054234	4153	t
+74054246	9111050010-K	URH állócsatlás 18 eres	2	74054246	4153	t
+74054250	9111030010-K	ESW csatlás mellső T5C5 korszer.javított	1	74054250	4216	t
+74054251	9111030011-K	ESW csatlás hátsó T5C5 korszer.Javított	1	74054251	4216	t
+74054261	9151040000-K	KONTAKTOR SL 11/A	1	74054261	4216	t
+74054274	9111050010-K	URH állócsatlás 18 eres	2	74054274	4216	t
 \.
