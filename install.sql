7d5f69744e967e6db57ae227f93bd6a832619a19
triton-dev <szikoragy@gmail.com>
2017-01-17 14:45:31 +0100

    Update install.sql

--- install.sql
+++ install.sql
@@ -21,11 +21,13 @@ create domain d_nyitott as boolean not null default true;
 create domain d_szekvenciaszam as varchar(25) default null;
 create domain d_sarzs as varchar(25) default null;
 
+-- Költséghelyek táblája
 create table ktghely (
 	ktghely d_ktghely primary key,
 	ktghelynev d_ktghelynev not null
 ) with oids;
 
+-- Dolgozók táblája
 create table dolgozo (
 	torzsszam d_torzsszam primary key,
 	nev d_dolgozonev not null,
@@ -34,6 +36,7 @@ create table dolgozo (
 	fenykep d_fajl
 ) with oids;
 
+-- Belépőkártyák táblája
 create table kartya (
 	kartyaszam d_kartyaszam primary key,
 	torzsszam d_torzsszam references dolgozo(torzsszam)
@@ -41,6 +44,7 @@ create table kartya (
 	allandokartya d_allandokartya
 ) with oids;
 
+-- Rendelések táblája
 create table rendeles (
 	rendelesszam d_rendelesszam primary key,
 	cikkszam d_cikkszam,
@@ -51,20 +55,45 @@ create table rendeles (
 	nyitott d_nyitott
 ) with oids;
 
+-- Melyik dolgozóhoz milyen rendelést kapcsolt a művezető és meddig.
 create table osszerendeles (
-  rendelesszam d_rendelesszam references rendeles(rendelesszam)
-    on update cascade on delete restrict,
-  torzsszam d_torzsszam references dolgozo (torzsszam)
-    on update cascade on delete restrict,
-  mettol date not null default current_date,
-  meddig date not null default current_date + 7,
-  primary key(rendelesszam, torzsszam)
+	rendelesszam d_rendelesszam references rendeles(rendelesszam)
+		on update cascade on delete restrict,
+	torzsszam d_torzsszam references dolgozo (torzsszam)
+		on update cascade on delete restrict,
+	mettol date not null default current_date,
+	meddig date not null default current_date + 7,
+		primary key(rendelesszam, torzsszam)
 ) with oids;
 
+
+-- Kinek mi a feladata nézet tábla
 create view kimitcsinal as
-select * from osszerendeles 
-  join dolgozo using(torzsszam) 
-  join rendeles using(rendelesszam) order by 1,2;
+	select * from osszerendeles 
+		join dolgozo using(torzsszam) 
+		join rendeles using(rendelesszam) 
+		where nyitott
+		order by 1,2;
+  
+
+-- Visszajelentések rögzítése  
+create table rogzit (
+	rendelesszam d_rendelesszam,
+	torzsszam d_torzsszam,
+	kezd	timestamp without time zone default current_timestamp,
+	vegez	timestamp without time zone default null,
+	foreign key (rendelesszam, torzsszam) 
+		references osszerendeles(rendelesszam, torzsszam)
+		on update cascade on delete restrict
+) with oids;
+
+
+-- Ez itt nem jó...
+-- create or replace rule visszajelent as on insert to rogzit 
+	-- where (new.rendelesszam = new.rendelesszam and new.torzsszam=new.torzsszam
+		-- and kezd is not null and vegez is null)
+	-- do instead
+	-- update rogzit set vegez=current_timestamp where rendelesszam=new.rendelesszam and torzsszam=new.torzsszam;
 
 -- Betöltések
 \i ktghely.sql
@@ -95,10 +124,13 @@ $$
 
 \i tesztkartya.sql
 
+-- Olyan rendelések lezárása, melyek szekvenciaszáma 0 (nulla).
+update rendeles set nyitott='f' where szekvenciaszam is null;
+
 -- osszerendeles tábla feltöltése
 insert into osszerendeles(torzsszam, rendelesszam) 
   select torzsszam,rendelesszam from dolgozo,rendeles 
-    where ktghely ilike'%425%' and rendelesszam ilike '71%' order by 1;
+	where ktghely ilike'%425%' and rendelesszam ilike '71%' and nyitott order by 1;
 	
 -- fényképek igazítása...
 do
@@ -111,8 +143,6 @@ $$
 $$
 ;
 
--- Olyan rendelések lezárása, melyek szekvenciaszáma 0 (nulla).
-update rendeles set nyitott='f' where szekvenciaszam='0';
 
 -- Kártyaszám ö-0 karakterek cseréjér
 -- A kártyaolvasó angol nyelvű billentyűzetet emulál...
@@ -122,5 +152,5 @@ begin
 	return left(replace($1,'ö','0'),10);
 end;
 $$
-language plpgsql;
+language 'plpgsql';
 
